5790447fe26518bb1d0765464a15846624fe9f2c
ahmedjutt112 <58397339+ahmedjutt112@users.noreply.github.com>
2020-12-08 11:55:08 +0500

    Create mousemove_in_python.py

---
+++ mousemove_in_python.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+import random
+
+# A simpal move mouse in pyautogui
+
+pytutogui.FILEFALES=Falaes
+
+while true:
+     time.sleep(5)
+		 for i in range(0 , 100):
+		     pyautogui.mouseTo(0 , i*10)
+				 
+				 
+				 
+# A random move mouse 
+
+"""
+
+while true :
+     
